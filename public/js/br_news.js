e09ee21efa6f124f8eeb8723fd072b7661f7d148
Alexandru Bostan <alexandru.bostan9@gmail.com>
2019-07-02 15:07:00 +0200

    Update Br News

--- public/js/br_news.js
+++ public/js/br_news.js
@@ -10,9 +10,9 @@
     const displayBrNews = () => {
         window.ApiService.searchNews()
             .then(result => displayBrNewsItems(result.entries))
-            .catch(() => $('#br-news-container').empty().append(
-                displayAlert(defaultAlertClasses, 'Sorry, but could not display the Battle Royale news.')
-            ))
+        // .catch(() => $('#br-news-container').empty().append(
+        //     displayAlert(defaultAlertClasses, 'Sorry, but could not display the Battle Royale news.')
+        // ))
     }
 
     const displayBrNewsItems = (entries) => $('#br-news-container').empty().append(
@@ -31,7 +31,7 @@
         }
         var time = element.time;
         time = toDateTime(time)
-        
+
         var myDate = new Date(time);
         var day = myDate.getDate();
         var month = myDate.getMonth();
@@ -40,19 +40,25 @@
         function pad(n) {
             return n < 10 ? '0' + n : n
         }
-
         var date_news = pad(day) + "/" + pad(month + 1) + "/" + year;
 
-        return $('<div></div>').addClass('card shadow p-3 mx-1 my-1 bg-white').height('80%').width('80%')
+        const label = element.meta.adSpace
+        const color = element.meta.mainColor
+        const colorLabel = (label !== null && color.length !== 7) ? color + color.substring(color.length - 1, color.length) : color
+        console.log(color)
+        console.log(colorLabel)
+
+        return $('<div></div>').addClass('card shadow p-3 mx-1 my-1 bg-white').height('80%').width('70%')
             .append($('<div></div>').addClass('card-container')
                 .append($('<a></a>').attr({ href: element.image, alt: element.title })
                     .append($('<img/>').addClass('card-img-top').attr({ src: element.image, alt: element.title }).height('100%').width('100%')))
                 .append($('<div></div>').addClass('card-body')
                     .append($('<div></div>')
+                        .append($('<small></small>').text(date_news).addClass('text-muted'))
+                        .append($('<span></span>').text(' '))
+                        .append($('<span></span>').text(element.meta.adSpace).addClass('badge').css({ 'background-color': colorLabel, 'color': 'white' }))
                         .append($('<h5></h5>').addClass('card-title text-primary').text(element.title))
                         .append($('<p></p>').text(element.body).addClass('text-justify'))
-                        .append($('<p></p>').text(date_news))
-                        .append($('<span></span>').text(element.meta.adSpace).addClass('badge badge-success'))
                     )
                 )
             );
